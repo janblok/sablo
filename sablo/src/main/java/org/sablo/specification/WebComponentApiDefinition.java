d050499cdb1ca790064b1ecd38bb4c6bc2581010
Edit Mera <emera@servoy.com>
2014-05-30 11:30:35 +0000

    add setFindMode for all servoy default components Case: SVY-6330

--- sablo/src/main/java/org/sablo/specification/WebComponentApiDefinition.java
+++ sablo/src/main/java/org/sablo/specification/WebComponentApiDefinition.java
@@ -33,7 +33,7 @@ public class WebComponentApiDefinition
 	private PropertyDescription returnType;
 	private JSONObject customConfigOptions;
 
-	WebComponentApiDefinition(String name)
+	public WebComponentApiDefinition(String name)
 	{
 		this.name = name;
 	}

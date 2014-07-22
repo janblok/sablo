8c8754abc7e5286d3f5f3ac357ec572e928a6ae4
jcompagner <jcompagner@servoy.com>
2014-07-22 14:35:36 +0200

    SVY-6615 add server side api support
    make flagPropertyChange public, so that the outside world can flag a
    property as changed if it is a complex/custom property and the object is
    changed internally

--- sablo/src/main/java/org/sablo/BaseWebObject.java
+++ sablo/src/main/java/org/sablo/BaseWebObject.java
@@ -26,7 +26,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.sablo.specification.PropertyDescription;
-import org.sablo.specification.WebComponentApiDefinition;
 import org.sablo.specification.WebComponentSpecProvider;
 import org.sablo.specification.WebComponentSpecification;
 import org.sablo.specification.property.DataConverterContext;
@@ -36,19 +35,17 @@ import org.sablo.specification.property.ICustomType;
 import org.sablo.specification.property.IPropertyType;
 import org.sablo.specification.property.IWrapperType;
 import org.sablo.websocket.ConversionLocation;
-import org.sablo.websocket.WebsocketEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * This is a base web object that hold properties and records changes for the specific {@link WebComponentSpecProvider}
- * 
+ *
  * @author jcompagner
  */
-public abstract class BaseWebObject 
+public abstract class BaseWebObject
 {
-	private static final Logger log = LoggerFactory
-			.getLogger(BaseWebObject.class.getCanonicalName());
+	private static final Logger log = LoggerFactory.getLogger(BaseWebObject.class.getCanonicalName());
 
 
 	protected final WebComponentSpecification specification;
@@ -64,60 +61,67 @@ public abstract class BaseWebObject
 	private final Set<String> changedProperties = new HashSet<>(3);
 
 	protected final String name;
-	
-	public BaseWebObject(String name, WebComponentSpecification specification) {
+
+	public BaseWebObject(String name, WebComponentSpecification specification)
+	{
 		this.name = name;
 		this.specification = specification;
-		if (specification == null)
-			throw new IllegalStateException(
-					"Cannot work without specification");
+		if (specification == null) throw new IllegalStateException("Cannot work without specification");
 	}
 
 	/**
 	 * Returns the component name
-	 * 
+	 *
 	 * @return the name
 	 */
-	public final String getName() {
+	public final String getName()
+	{
 		return name;
 	}
-	
+
 	/**
 	 * Execute incoming event
-	 * 
+	 *
 	 * @param eventType
 	 * @param args
 	 * @return
 	 */
-	public Object executeEvent(String eventType, Object[] args) {
+	public Object executeEvent(String eventType, Object[] args)
+	{
 		return null;
 	}
 
 	/**
 	 * Get a property
-	 * 
+	 *
 	 * @param propertyName
 	 * @return the value or null
 	 */
-	public Object getProperty(String propertyName) {
+	public Object getProperty(String propertyName)
+	{
 		Object object = properties.get(propertyName);
-		if (object != null) {
-			PropertyDescription propDesc = specification
-					.getProperty(propertyName);
-			if (propDesc != null) {
-				IPropertyType<?> type = propDesc.getType();
-				if (type instanceof IWrapperType) {
-					return ((IWrapperType) type).unwrap(object);
+		if (object != null)
+		{
+			PropertyDescription propDesc = specification.getProperty(propertyName);
+			if (propDesc != null)
+			{
+				IPropertyType< ? > type = propDesc.getType();
+				if (type instanceof IWrapperType)
+				{
+					return ((IWrapperType)type).unwrap(object);
 				}
 			}
 		}
 		return object;
 	}
 
-	public Map<String, Object> getChanges() {
-		if (changedProperties.size() > 0) {
+	public Map<String, Object> getChanges()
+	{
+		if (changedProperties.size() > 0)
+		{
 			Map<String, Object> changes = new HashMap<>();
-			for (String propertyName : changedProperties) {
+			for (String propertyName : changedProperties)
+			{
 				changes.put(propertyName, properties.get(propertyName));
 			}
 			changedProperties.clear();
@@ -126,30 +130,34 @@ public abstract class BaseWebObject
 		return Collections.emptyMap();
 	}
 
-	public Map<String, Object> getProperties() {
+	public Map<String, Object> getProperties()
+	{
 		return properties;
 	}
 
-	public void clearChanges() {
+	public void clearChanges()
+	{
 		changedProperties.clear();
 	}
 
 	/**
 	 * Setting new data and recording this as change.
-	 * 
+	 *
 	 * @param propertyName
 	 * @param propertyValue
 	 * @return true is was change
 	 */
-	public boolean setProperty(String propertyName, Object propertyValue,
-			ConversionLocation sourceOfValue) {
+	public boolean setProperty(String propertyName, Object propertyValue, ConversionLocation sourceOfValue)
+	{
 		Map<String, Object> map = properties;
-		try {
+		try
+		{
 			// TODO can the propertyName can contain dots? Or should this be
 			// handled by the type??
-			propertyValue = wrapPropertyValue(propertyName,
-					map.get(propertyName), propertyValue);
-		} catch (Exception e) {
+			propertyValue = wrapPropertyValue(propertyName, map.get(propertyName), propertyValue);
+		}
+		catch (Exception e)
+		{
 			// TODO change this as part of SVY-6337
 			throw new RuntimeException(e);
 		}
@@ -159,12 +167,14 @@ public abstract class BaseWebObject
 		String firstPropertyPart = propertyName;
 		String lastPropertyPart = propertyName;
 		String[] parts = propertyName.split("\\.");
-		if (parts.length > 1) {
+		if (parts.length > 1)
+		{
 			firstPropertyPart = parts[0];
-			for (int i = 0; i < parts.length - 1; i++) {
-				Map<String, Object> propertyMap = (Map<String, Object>) map
-						.get(parts[i]);
-				if (propertyMap == null) {
+			for (int i = 0; i < parts.length - 1; i++)
+			{
+				Map<String, Object> propertyMap = (Map<String, Object>)map.get(parts[i]);
+				if (propertyMap == null)
+				{
 					propertyMap = new HashMap<>();
 					map.put(parts[i], propertyMap);
 				}
@@ -173,18 +183,20 @@ public abstract class BaseWebObject
 			lastPropertyPart = parts[parts.length - 1];
 		}
 
-		if (map.containsKey(lastPropertyPart)) {
+		if (map.containsKey(lastPropertyPart))
+		{
 			// existing property
 			Object oldValue = map.put(lastPropertyPart, propertyValue);
 			onPropertyChange(firstPropertyPart, oldValue, propertyValue);
 
-			if ((oldValue != null && !oldValue.equals(propertyValue))
-					|| (propertyValue != null && !propertyValue
-							.equals(oldValue))) {
+			if ((oldValue != null && !oldValue.equals(propertyValue)) || (propertyValue != null && !propertyValue.equals(oldValue)))
+			{
 				changedProperties.add(firstPropertyPart);
 				return true;
 			}
-		} else {
+		}
+		else
+		{
 			// new property
 			map.put(lastPropertyPart, propertyValue);
 			onPropertyChange(firstPropertyPart, null, propertyValue);
@@ -198,13 +210,13 @@ public abstract class BaseWebObject
 	/**
 	 * put property from the outside world, not recording changes. converting to
 	 * the right type.
-	 * 
+	 *
 	 * @param propertyName
 	 * @param propertyValue
 	 *            can be a JSONObject or array or primitive.
 	 */
-	public void putBrowserProperty(String propertyName, Object propertyValue)
-			throws JSONException {
+	public void putBrowserProperty(String propertyName, Object propertyValue) throws JSONException
+	{
 		// currently we keep Java objects in here; we could switch to having
 		// only json objects in here is it make things quicker
 		// (then whenever a server-side value is put in the map, convert it via
@@ -222,13 +234,12 @@ public abstract class BaseWebObject
 		// }
 		// }// end TODO REMOVE
 		Object oldValue = properties.get(propertyName);
-		properties.put(propertyName,convertValueAndWrap(propertyName, oldValue, propertyValue,
-								ConversionLocation.BROWSER_UPDATE));
+		properties.put(propertyName, convertValueAndWrap(propertyName, oldValue, propertyValue, ConversionLocation.BROWSER_UPDATE));
 	}
 
 	/**
 	 * Allow for subclasses to act on property changes
-	 * 
+	 *
 	 * @param propertyName
 	 *            the property name
 	 * @param oldValue
@@ -236,33 +247,30 @@ public abstract class BaseWebObject
 	 * @param newValue
 	 *            the new val
 	 */
-	protected void onPropertyChange(String propertyName, Object oldValue,
-			Object newValue) {
+	protected void onPropertyChange(String propertyName, Object oldValue, Object newValue)
+	{
 	}
 
 	/**
 	 * @param key
 	 */
-	protected void flagPropertyChanged(String key) {
+	public void flagPropertyChanged(String key)
+	{
 		changedProperties.add(key);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Object wrapPropertyValue(String propertyName, Object oldValue,
-			Object newValue) throws JSONException {
-		PropertyDescription propertyDesc = specification
-				.getProperty(propertyName);
-		IPropertyType<Object> type = propertyDesc != null ? (IPropertyType<Object>) propertyDesc
-				.getType() : null;
-		Object object = (type instanceof IWrapperType) ? ((IWrapperType) type)
-				.wrap(newValue, oldValue, new DataConverterContext(propertyDesc,this)) : newValue;
-		if (type instanceof IClassPropertyType && object != null) {
-			if (!((IClassPropertyType<?,?>) type).getTypeClass().isAssignableFrom(object
-					.getClass())) {
-				log.info("property: " + propertyName + " of component "
-						+ getName() + " set with value: " + newValue
-						+ " which is not of type: "
-						+ ((IClassPropertyType<?,?>) type).getTypeClass());
+	private Object wrapPropertyValue(String propertyName, Object oldValue, Object newValue) throws JSONException
+	{
+		PropertyDescription propertyDesc = specification.getProperty(propertyName);
+		IPropertyType<Object> type = propertyDesc != null ? (IPropertyType<Object>)propertyDesc.getType() : null;
+		Object object = (type instanceof IWrapperType) ? ((IWrapperType)type).wrap(newValue, oldValue, new DataConverterContext(propertyDesc, this)) : newValue;
+		if (type instanceof IClassPropertyType && object != null)
+		{
+			if (!((IClassPropertyType< ? , ? >)type).getTypeClass().isAssignableFrom(object.getClass()))
+			{
+				log.info("property: " + propertyName + " of component " + getName() + " set with value: " + newValue + " which is not of type: " +
+					((IClassPropertyType< ? , ? >)type).getTypeClass());
 				return null;
 			}
 		}
@@ -272,7 +280,7 @@ public abstract class BaseWebObject
 	/**
 	 * Allow for subclasses to do conversions, by default it just ask for the
 	 * type to do the conversion to Java
-	 * 
+	 *
 	 * @param propertyName
 	 *            the property name
 	 * @param oldValue
@@ -283,18 +291,13 @@ public abstract class BaseWebObject
 	 * @return the converted value
 	 * @throws JSONException
 	 */
-	private Object convertValueAndWrap(String propertyName, Object oldValue,
-			Object newValue, ConversionLocation sourceOfValue)
-			throws JSONException {
-		if (newValue == null || newValue == JSONObject.NULL)
-			return null;
+	private Object convertValueAndWrap(String propertyName, Object oldValue, Object newValue, ConversionLocation sourceOfValue) throws JSONException
+	{
+		if (newValue == null || newValue == JSONObject.NULL) return null;
 
-		PropertyDescription propertyDesc = specification
-				.getProperty(propertyName);
-		Object value = propertyDesc != null ? convertPropertyValueAndWrap(oldValue, newValue, propertyDesc)
-				: null;
-		return value != null && value != newValue ? value : convertPropertyValue(propertyName,
-				oldValue, newValue, sourceOfValue);
+		PropertyDescription propertyDesc = specification.getProperty(propertyName);
+		Object value = propertyDesc != null ? convertPropertyValueAndWrap(oldValue, newValue, propertyDesc) : null;
+		return value != null && value != newValue ? value : convertPropertyValue(propertyName, oldValue, newValue, sourceOfValue);
 	}
 
 	/**
@@ -303,28 +306,35 @@ public abstract class BaseWebObject
 	 * @param type
 	 * @throws JSONException
 	 */
-	private Object convertPropertyValueAndWrap(Object oldValue, Object newValue,
-			PropertyDescription propDesc) throws JSONException {
+	private Object convertPropertyValueAndWrap(Object oldValue, Object newValue, PropertyDescription propDesc) throws JSONException
+	{
 
-		if (propDesc.isArray()) {
-			if (propDesc.getType() instanceof IComplexTypeImpl && ((IComplexTypeImpl)propDesc.getType()).getJSONToJavaPropertyConverter(true) != null) {
+		if (propDesc.isArray())
+		{
+			if (propDesc.getType() instanceof IComplexTypeImpl && ((IComplexTypeImpl)propDesc.getType()).getJSONToJavaPropertyConverter(true) != null)
+			{
 				return newValue; // this is currently handled elsewhere (when setting property in component)
 			}
-			
-			if (newValue instanceof JSONArray) {
-				JSONArray array = (JSONArray) newValue;
+
+			if (newValue instanceof JSONArray)
+			{
+				JSONArray array = (JSONArray)newValue;
 				Object[] objectArray = new Object[array.length()];
-				for(int i=0;i<array.length();i++) {
+				for (int i = 0; i < array.length(); i++)
+				{
 					Object obj = array.opt(i);
-					objectArray[i] = obj == null?null:convertValueAndWrap(null,obj, propDesc);
+					objectArray[i] = obj == null ? null : convertValueAndWrap(null, obj, propDesc);
 				}
 				return objectArray;
 			}
-			else {
-				throw new RuntimeException("property "+ propDesc +" is types as array, but the value is not an JSONArray: " + newValue );
+			else
+			{
+				throw new RuntimeException("property " + propDesc + " is types as array, but the value is not an JSONArray: " + newValue);
 			}
 
-		} else {
+		}
+		else
+		{
 			return convertValueAndWrap(oldValue, newValue, propDesc);
 		}
 	}
@@ -336,39 +346,42 @@ public abstract class BaseWebObject
 	 * @throws JSONException
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private Object convertValueAndWrap(Object oldValue, Object newValue,
-			PropertyDescription desc) throws JSONException {
+	private Object convertValueAndWrap(Object oldValue, Object newValue, PropertyDescription desc) throws JSONException
+	{
 		if (newValue == null || newValue == JSONObject.NULL) return null;
-		IPropertyType<?> type = desc.getType();
-		if (type instanceof IClassPropertyType) {
-			Object fromJSON = ((IClassPropertyType) type).fromJSON(newValue, oldValue);
-			if (type instanceof IWrapperType) {
-				return ((IWrapperType) type).wrap(fromJSON, oldValue, new DataConverterContext(desc, this));
+		IPropertyType< ? > type = desc.getType();
+		if (type instanceof IClassPropertyType)
+		{
+			Object fromJSON = ((IClassPropertyType)type).fromJSON(newValue, oldValue);
+			if (type instanceof IWrapperType)
+			{
+				return ((IWrapperType)type).wrap(fromJSON, oldValue, new DataConverterContext(desc, this));
 			}
 			return fromJSON;
-		} else if (type instanceof IComplexTypeImpl) {
+		}
+		else if (type instanceof IComplexTypeImpl)
+		{
 			return newValue; // this is currently handled elsewhere (when setting property in component)
-		} else if (type instanceof ICustomType) {
+		}
+		else if (type instanceof ICustomType)
+		{
 			// custom type, convert json to map with values.
-			if (newValue instanceof JSONObject) {
+			if (newValue instanceof JSONObject)
+			{
 				Map<String, Object> retValue = new HashMap<>();
-				Map<String, Object> oldValues = (Map<String, Object>) (oldValue instanceof Map ? oldValue
-						: Collections.emptyMap());
-				PropertyDescription customTypeDesc = ((ICustomType) type)
-						.getCustomJSONTypeDefinition();
-				Iterator<String> keys = ((JSONObject) newValue).keys();
-				while (keys.hasNext()) {
+				Map<String, Object> oldValues = (Map<String, Object>)(oldValue instanceof Map ? oldValue : Collections.emptyMap());
+				PropertyDescription customTypeDesc = ((ICustomType)type).getCustomJSONTypeDefinition();
+				Iterator<String> keys = ((JSONObject)newValue).keys();
+				while (keys.hasNext())
+				{
 					String key = keys.next();
-					Object propValue = ((JSONObject) newValue).get(key);
+					Object propValue = ((JSONObject)newValue).get(key);
 					Object oldPropValue = oldValues.get(key);
-					PropertyDescription property = customTypeDesc
-							.getProperty(key);
-					if (property == null)
-						continue; // ignore properties that are not spec'ed
-									// for
-									// this type..
-					Object value = convertPropertyValueAndWrap(oldPropValue, propValue,
-							property);
+					PropertyDescription property = customTypeDesc.getProperty(key);
+					if (property == null) continue; // ignore properties that are not spec'ed
+													// for
+													// this type..
+					Object value = convertPropertyValueAndWrap(oldPropValue, propValue, property);
 					retValue.put(key, value);
 				}
 				return retValue;
@@ -380,7 +393,7 @@ public abstract class BaseWebObject
 	/**
 	 * Allow for subclasses to do conversions, by default it just ask for the
 	 * type to do the conversion to Java
-	 * 
+	 *
 	 * @param propertyName
 	 *            the property name
 	 * @param oldValue
@@ -391,18 +404,17 @@ public abstract class BaseWebObject
 	 * @return the converted value
 	 * @throws JSONException
 	 */
-	protected Object convertPropertyValue(String propertyName, Object oldValue,
-			Object newValue, ConversionLocation sourceOfValue)
-			throws JSONException {
+	protected Object convertPropertyValue(String propertyName, Object oldValue, Object newValue, ConversionLocation sourceOfValue) throws JSONException
+	{
 		return newValue;
 	}
-	
+
 	/**
 	 * Called when this object will not longer be used - to release any held resources/remove listeners.
 	 */
 	public void dispose()
 	{
-		
+
 	}
 
 }

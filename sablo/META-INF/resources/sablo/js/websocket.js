3c56789fca34c4b8164a91941da689006d15efa0
georgeganea <gganea@servoy.com>
2015-01-29 11:42:09 +0200

    SVY-6956 call to getInDepthProperty is now being done correctly

--- sablo/META-INF/resources/sablo/js/websocket.js
+++ sablo/META-INF/resources/sablo/js/websocket.js
@@ -635,7 +635,10 @@ webSocketModule.factory('$webSocket',
 				generateWatchFunctionFor: function (modelObjectRoot, path) {
 									var filteredObject = function (scope) {
 														var result = {};
-														var modelObject = sabloUtils.getInDepthProperty(modelObjectRoot, path)
+														var args = [];
+														args.push(modelObjectRoot);
+														args = args.concat(path);
+														var modelObject = sabloUtils.getInDepthProperty.apply(sabloUtils,args);
 														
 														for (k in modelObject) {
 															if (modelObject[k].__internalState && modelObject[k].__internalState.setChangeNotifier) {

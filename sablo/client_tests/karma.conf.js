b3f62006aadee2f861509c3fc08ec6061549d712
Gabi Boros <gboros@servoy.com>
2015-01-30 17:18:14 +0200

    SVY-7517 Set up Sablo karma/jasmine tests

---
+++ sablo/client_tests/karma.conf.js
@@ -0,0 +1,35 @@
+module.exports = function(config){
+  config.set({
+    basePath : '.',
+    files : [
+       'lib/jquery.js',
+       'lib/angular_1.3.4.js',
+       'lib/angular-mocks_1.3.4.js',
+       '../META-INF/resources/sablo/js/*.js',
+       'lib/phantomjs.polyfill.js',
+       './test/**/*.js',	  
+    ],
+
+    frameworks: ['jasmine'],
+    browsers : ['PhantomJS'],
+
+    /*plugins : [    <- not needed since karma loads by default all sibling plugins that start with karma-*
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-script-launcher',
+            'karma-jasmine'
+            ],*/
+    singleRun: true,
+    //autoWatch : true,
+    reporters: ['dots', 'junit'],
+    junitReporter: {
+          outputFile: 'test-results.xml'
+    }
+  /*,  alternative format
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }*/
+  });
+};
